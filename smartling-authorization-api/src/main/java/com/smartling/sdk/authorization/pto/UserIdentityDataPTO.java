16312327311069f368395e40285b0c13bbfac900
Scott Rossillo <srossillo@smartling.com>
2019-11-02 10:44:01 -0400

    feat: add authorization api

---
+++ smartling-authorization-api/src/main/java/com/smartling/sdk/authorization/pto/UserIdentityDataPTO.java
@@ -0,0 +1,18 @@
+package com.smartling.sdk.authorization.pto;
+
+import com.smartling.web.api.v2.ResponseData;
+
+public class UserIdentityDataPTO implements ResponseData
+{
+    private UserIdentityPTO userIdentity;
+
+    public UserIdentityPTO getUserIdentity()
+    {
+        return userIdentity;
+    }
+
+    public void setUserIdentity(UserIdentityPTO userIdentity)
+    {
+        this.userIdentity = userIdentity;
+    }
+}
---
+++ smartling-authorization-api/src/main/java/com/smartling/sdk/authorization/pto/UserIdentityPTO.java
@@ -0,0 +1,68 @@
+package com.smartling.sdk.authorization.pto;
+
+import com.smartling.web.api.v2.ResponseData;
+
+
+public class UserIdentityPTO implements ResponseData
+{
+    private String accountUid;
+    private String projectUid;
+    private String agencyUid;
+    private String userUid;
+    private String roleName;
+
+    public String getAccountUid()
+    {
+        return accountUid;
+    }
+
+    public UserIdentityPTO setAccountUid(String accountUid)
+    {
+        this.accountUid = accountUid;
+        return this;
+    }
+
+    public String getProjectUid()
+    {
+        return projectUid;
+    }
+
+    public UserIdentityPTO setProjectUid(String projectUid)
+    {
+        this.projectUid = projectUid;
+        return this;
+    }
+
+    public String getAgencyUid()
+    {
+        return agencyUid;
+    }
+
+    public UserIdentityPTO setAgencyUid(String agencyUid)
+    {
+        this.agencyUid = agencyUid;
+        return this;
+    }
+
+    public String getUserUid()
+    {
+        return userUid;
+    }
+
+    public UserIdentityPTO setUserUid(String userUid)
+    {
+        this.userUid = userUid;
+        return this;
+    }
+
+    public String getRoleName()
+    {
+        return roleName;
+    }
+
+    public UserIdentityPTO setRoleName(String roleName)
+    {
+        this.roleName = roleName;
+        return this;
+    }
+}
