6b0a1fa4aeed25b65e62b0c5b9925d554fb421d4
Scott Rossillo <srossillo@smartling.com>
2019-11-08 15:12:55 -0500

    Prep for release to Maven Central

---
+++ .circleci/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Checking if we should release"
+if ! ./mvnw -Prelease conventional-commits:validate; then
+    echo "No changes to release"
+    exit 0
+fi
+
+exit 0
+./mvnw -B -Prelease -DskipTests -Darguments='-DskipTests' -s .circleci/settings.xml conventional-commits:version release:prepare
+./mvnw -B -DskipTests -Darguments=-DskipTests -s .circleci/settings.xml release:perform
