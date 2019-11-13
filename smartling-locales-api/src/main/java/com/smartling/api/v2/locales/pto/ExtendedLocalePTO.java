359f5265f14b14a8491d8b75924a91deddc84412
Scott Rossillo <srossillo@smartling.com>
2019-11-13 10:42:24 -0500

    Add locales SDK to aggregator

--- smartling-locales-api/src/main/java/com/smartling/api/v2/locales/pto/ExtendedLocalePTO.java
+++ smartling-locales-api/src/main/java/com/smartling/api/v2/locales/pto/ExtendedLocalePTO.java
@@ -1,6 +1,7 @@
 package com.smartling.api.v2.locales.pto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * PTO class to provide basic info about locale, which is Id and Description.
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 public class ExtendedLocalePTO extends LocalePTO
