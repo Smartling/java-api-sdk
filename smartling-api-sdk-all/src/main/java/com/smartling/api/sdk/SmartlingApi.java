9621cbf65f254a2c58cf14ae4c19bb79019690f1
Igor Shulyak <ishulyak@smartling.com>
2019-11-13 15:29:33 +0200

    JOBS-2616 Pom change

--- smartling-api-sdk-all/src/main/java/com/smartling/api/sdk/SmartlingApi.java
+++ smartling-api-sdk-all/src/main/java/com/smartling/api/sdk/SmartlingApi.java
@@ -1,6 +1,7 @@
 package com.smartling.api.sdk;
 
 import com.smartling.api.v2.issues.IssuesApi;
+import com.smartling.api.v3.jobs.TranslationJobApi;
 import com.smartling.api.locales.v2.LocalesApi;
 
 /**
@@ -23,4 +24,11 @@ public interface SmartlingApi
      * @return {@link LocalesApi}
      */
     LocalesApi localesApi();
+    /**
+     * Returns an API to for working with Smartling
+     * <a href="https://api-reference.smartling.com/#tag/Jobs">issues</a>.
+     *
+     * @return {@link TranslationJobApi}
+     */
+    TranslationJobApi translationJobApi();
 }
