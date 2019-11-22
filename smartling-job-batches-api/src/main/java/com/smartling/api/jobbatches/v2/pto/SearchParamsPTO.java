39d195fe3718fe0fb28297b95246f1b3eb3c1acf
Denis Kurochkin <dkurochkin@smartling.com>
2019-11-22 20:24:09 +0200

    JOBSBATCH-123 fields formatting

--- smartling-job-batches-api/src/main/java/com/smartling/api/jobbatches/v2/pto/SearchParamsPTO.java
+++ smartling-job-batches-api/src/main/java/com/smartling/api/jobbatches/v2/pto/SearchParamsPTO.java
@@ -15,14 +15,19 @@ public class SearchParamsPTO
 {
     @QueryParam("offset")
     private Integer offset;
+
     @QueryParam("limit")
     private Integer limit;
+
     @QueryParam("sortBy")
-    private String sortBy ;
+    private String sortBy;
+
     @QueryParam("orderBy")
     private String orderBy;
+
     @QueryParam("status")
     private String status;
+
     @QueryParam("translationJobUid")
     private String translationJobUid;
 }
