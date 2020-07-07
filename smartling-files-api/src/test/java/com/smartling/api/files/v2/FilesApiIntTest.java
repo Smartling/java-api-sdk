4a354ae2b17d88e0e38f4efb91f8f4f4bd9644b2
Denis Kurochkin <dkurochkin@smartling.com>
2020-07-07 16:53:27 +0300

    test: test for file upload

--- smartling-files-api/src/test/java/com/smartling/api/files/v2/FilesApiIntTest.java
+++ smartling-files-api/src/test/java/com/smartling/api/files/v2/FilesApiIntTest.java
@@ -2,6 +2,8 @@ package com.smartling.api.files.v2;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.smartling.api.files.v2.pto.DownloadTranslationPTO;
+import com.smartling.api.files.v2.pto.UploadFilePTO;
+import com.smartling.api.files.v2.pto.UploadFileResponse;
 import com.smartling.api.v2.client.ClientConfiguration;
 import com.smartling.api.v2.client.DefaultClientConfiguration;
 import com.smartling.api.v2.client.auth.BearerAuthStaticTokenFilter;
@@ -10,14 +12,24 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aMultipart;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.smartling.api.files.v2.SmartlingWireMock.success;
+import static com.smartling.api.files.v2.pto.FileType.JSON;
 import static com.smartling.api.files.v2.pto.RetrievalType.PUBLISHED;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
@@ -70,4 +82,68 @@ public class FilesApiIntTest
         assertEquals(IOUtils.toString(response, UTF_8), rawBody);
     }
 
+    @Test
+    public void shouldUploadFile() throws Exception
+    {
+        String rawBody = UUID.randomUUID().toString();
+
+        smartlingApi.stubFor(post(urlPathMatching("/files-api/v2/projects/.+/file"))
+            // language=JSON
+            .willReturn(success( "{\n" +
+                    "    \"overWritten\": true,\n" +
+                    "    \"stringCount\": 10,\n" +
+                    "    \"wordCount\": 3,\n" +
+                    "    \"message\" : \"uploaded\"\n" +
+                    "}")
+                )
+            );
+
+        Map<String, String> directives = new HashMap<>();
+        directives.put("dir.name.1", "dirValue1");
+        directives.put("dir.name.2", "dirValue2");
+
+        UploadFileResponse uploadFileResponse = filesApi.uploadFile(PROJECT_ID, UploadFilePTO.builder()
+            .authorize(true)
+            .fileType(JSON)
+            .fileUri(FILE_URI)
+            .file(new ByteArrayInputStream(rawBody.getBytes()))
+            .directives(directives)
+            .localeIdsToAuthorize(Arrays.asList("de-DE", "fr"))
+            .build());
+
+        smartlingApi.verify(postRequestedFor(urlEqualTo("/files-api/v2/projects/" + PROJECT_ID + "/file"))
+            .withRequestBodyPart(aMultipart()
+                .withName("dir.name.1")
+                .withBody(equalTo("dirValue1"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("dir.name.2")
+                .withBody(equalTo("dirValue2"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("fileUri")
+                .withBody(equalTo(FILE_URI))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("authorize")
+                .withBody(equalTo("true"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("fileType")
+                .withBody(equalTo("JSON"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("localeIdsToAuthorize[]")
+                .withBody(equalTo("de-DE"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("localeIdsToAuthorize[]")
+                .withBody(equalTo("fr"))
+                .build())
+            .withRequestBodyPart(aMultipart()
+                .withName("file")
+                .withBody(equalTo(rawBody))
+                .build())
+        );
+    }
 }
