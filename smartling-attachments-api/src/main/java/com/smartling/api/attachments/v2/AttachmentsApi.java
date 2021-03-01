2cea0e4be50cbb3ef0a42dbb0f2feec74950ba4e
Alexander Novak <alexn@archer-soft.com>
2021-03-01 16:57:57 +0200

    TCM-2256 Attachments API SDK

--- smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
+++ smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
@@ -1,19 +1,14 @@
 package com.smartling.api.attachments.v2;
 
-import com.smartling.api.attachments.v2.pto.AttachmentEntitiesPTO;
-import com.smartling.api.attachments.v2.pto.AttachmentListFilter;
 import com.smartling.api.attachments.v2.pto.AttachmentPTO;
 import com.smartling.api.attachments.v2.pto.EntityAttachmentsPTO;
 import com.smartling.api.attachments.v2.pto.EntityLinkCommand;
-import com.smartling.api.attachments.v2.pto.EntityListCommand;
 import com.smartling.api.v2.response.ListResponse;
 import com.smartling.api.v2.response.EmptyData;
 
-import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -25,10 +20,6 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface AttachmentsApi
 {
-    @GET
-    @Path("/accounts/{accountUid}/{type}/attachments")
-    ListResponse<AttachmentEntitiesPTO> listAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @BeanParam AttachmentListFilter listFilter);
-
     @PUT
     @Path("/accounts/{accountUid}/{type}")
     ListResponse<EntityAttachmentsPTO> linkAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, EntityLinkCommand command);
@@ -37,15 +28,7 @@ public interface AttachmentsApi
     @Path("/accounts/{accountUid}/{type}/{entityUid}")
     ListResponse<AttachmentPTO> entityAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid);
 
-    @POST
-    @Path("/accounts/{accountUid}/{type}")
-    ListResponse<EntityAttachmentsPTO> bulkEntityAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, EntityListCommand command);
-
     @DELETE
     @Path("/accounts/{accountUid}/{type}/{entityUid}")
     EmptyData deleteAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid);
-
-    @DELETE
-    @Path("/accounts/{accountUid}/{type}/{entityUid}/attachment/{attachmentUid}")
-    EmptyData deleteAttachment(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid, @PathParam("attachmentUid") String attachmentUid);
 }
--- smartling-attachments-api/src/test/java/com/smartling/api/attachments/v2/AttachmentsApiTest.java
+++ smartling-attachments-api/src/test/java/com/smartling/api/attachments/v2/AttachmentsApiTest.java
@@ -2,14 +2,10 @@ package com.smartling.api.attachments.v2;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.smartling.api.attachments.v2.pto.AttachmentEntitiesPTO;
-import com.smartling.api.attachments.v2.pto.AttachmentListFilter;
 import com.smartling.api.attachments.v2.pto.AttachmentPTO;
 import com.smartling.api.attachments.v2.pto.AttachmentType;
 import com.smartling.api.attachments.v2.pto.EntityAttachmentsPTO;
 import com.smartling.api.attachments.v2.pto.EntityLinkCommand;
-import com.smartling.api.attachments.v2.pto.EntityListCommand;
-import com.smartling.api.attachments.v2.pto.EntityPTO;
 import com.smartling.api.attachments.v2.pto.LinkedAttachmentPTO;
 import com.smartling.api.v2.client.ClientConfiguration;
 import com.smartling.api.v2.client.DefaultClientConfiguration;
@@ -47,12 +43,9 @@ public class AttachmentsApiTest
     private static final String IDENTITY_URL = "/accounts" + "/{" + ACCOUNT_UID_PARAMETER + "}" + "/{" + TYPE_PARAMETER + "}";
     private static final String LINKS = IDENTITY_URL;
     private static final String ENTITY_LINKS = LINKS + "/{" + ENTITY_UID_PARAMETER + "}";
-    private static final String ENTITY_ATTACHMENT = ENTITY_LINKS + "/attachment/{" + ATTACHMENT_UID_PARAMETER + "}";
-    private static final String ATTACHMENTS = IDENTITY_URL + "/attachments";
     private static final String BEARER_TOKEN = UUID.randomUUID().toString();
     private static final String ACCOUNT_UID = "account_uid";
     private static final String ENTITY_UID = "entity_uid";
-    private static final String ATTACHMENT_UID = "attachment_uid";
     private static final String ATTACHMENT_TYPE = AttachmentType.JOBS.name().toLowerCase();
     private static final List<String> PATH_PLACEHOLDERS = Arrays.asList(ACCOUNT_UID_PARAMETER, TYPE_PARAMETER, ENTITY_UID_PARAMETER, ATTACHMENT_UID_PARAMETER);
 
@@ -99,14 +92,6 @@ public class AttachmentsApiTest
         return json(new RestApiResponse<>(new SuccessResponse<>(responseData)));
     }
 
-    private EntityPTO createEntityPTO()
-    {
-        EntityPTO entityPTO = new EntityPTO();
-        entityPTO.setEntityUid(UUID.randomUUID().toString());
-        entityPTO.setCreatedByUserUid(UUID.randomUUID().toString());
-        return entityPTO;
-    }
-
     private void randomAttachmentPTO(AttachmentPTO attachmentPTO)
     {
         attachmentPTO.setAttachmentUid(UUID.randomUUID().toString());
@@ -128,14 +113,6 @@ public class AttachmentsApiTest
         LinkedAttachmentPTO linkedAttachmentPTO = new LinkedAttachmentPTO();
         randomAttachmentPTO(linkedAttachmentPTO);
         return linkedAttachmentPTO;
-    }
-
-    private AttachmentEntitiesPTO createAttachmentEntitiesPTO()
-    {
-        AttachmentEntitiesPTO attachmentEntitiesPTO = new AttachmentEntitiesPTO();
-        randomAttachmentPTO(attachmentEntitiesPTO);
-        attachmentEntitiesPTO.setEntities(Arrays.asList(createEntityPTO(), createEntityPTO()));
-        return attachmentEntitiesPTO;
     }
 
     private EntityAttachmentsPTO createEntityAttachmentsPTO()
@@ -180,30 +157,6 @@ public class AttachmentsApiTest
     }
 
     @Test
-    public void testListAttachments() throws Exception
-    {
-        AttachmentEntitiesPTO expectedPTO1 = createAttachmentEntitiesPTO();
-        AttachmentEntitiesPTO expectedPTO2 = createAttachmentEntitiesPTO();
-
-        assignResponse(HttpStatus.SC_OK, MediaType.APPLICATION_JSON, jsonResponse(new ListResponse<>(Arrays.asList(expectedPTO1, expectedPTO2))));
-
-        AttachmentListFilter attachmentListFilter = new AttachmentListFilter();
-        attachmentListFilter.setLimit(10);
-        attachmentListFilter.setOffset(5);
-
-        ListResponse<AttachmentEntitiesPTO> response = attachmentsApi.listAttachments(ACCOUNT_UID, ATTACHMENT_TYPE, attachmentListFilter);
-        RecordedRequest request = getRequestWithValidation(HttpMethod.GET, ATTACHMENTS, ACCOUNT_UID, ATTACHMENT_TYPE);
-        assertTrue(request.getPath().contains("offset=" + attachmentListFilter.getOffset()));
-        assertTrue(request.getPath().contains("limit=" + attachmentListFilter.getLimit()));
-        assertEquals(2, response.getItems().size());
-
-        AttachmentEntitiesPTO actualPTO1 = response.getItems().get(0);
-        AttachmentEntitiesPTO actualPTO2 = response.getItems().get(1);
-        assertEquals(expectedPTO1.getAttachmentUid(), actualPTO1.getAttachmentUid());
-        assertEquals(expectedPTO2.getAttachmentUid(), actualPTO2.getAttachmentUid());
-    }
-
-    @Test
     public void testLinkAttachments() throws Exception
     {
         EntityAttachmentsPTO expectedPTO1 = createEntityAttachmentsPTO();
@@ -245,42 +198,11 @@ public class AttachmentsApiTest
     }
 
     @Test
-    public void testBulkEntityAttachments() throws Exception
-    {
-        EntityAttachmentsPTO expectedPTO1 = createEntityAttachmentsPTO();
-        EntityAttachmentsPTO expectedPTO2 = createEntityAttachmentsPTO();
-
-        assignResponse(HttpStatus.SC_OK, MediaType.APPLICATION_JSON, jsonResponse(new ListResponse<>(Arrays.asList(expectedPTO1, expectedPTO2))));
-
-        EntityListCommand command = new EntityListCommand();
-        command.setEntityUids(Arrays.asList("e1", "e2"));
-
-        ListResponse<EntityAttachmentsPTO> response = attachmentsApi.bulkEntityAttachments(ACCOUNT_UID, ATTACHMENT_TYPE, command);
-        RecordedRequest request = getRequestWithValidation(HttpMethod.POST, LINKS, ACCOUNT_UID, ATTACHMENT_TYPE);
-        assertEquals(2, response.getItems().size());
-        assertEquals(json(command), request.getBody().readUtf8());
-
-        EntityAttachmentsPTO actualPTO1 = response.getItems().get(0);
-        EntityAttachmentsPTO actualPTO2 = response.getItems().get(1);
-        assertEquals(expectedPTO1.getEntityUid(), actualPTO1.getEntityUid());
-        assertEquals(expectedPTO2.getEntityUid(), actualPTO2.getEntityUid());
-    }
-
-    @Test
     public void testDeleteAttachments() throws Exception
     {
         assignResponse(HttpStatus.SC_OK, MediaType.APPLICATION_JSON, jsonResponse(null));
 
         assertNotNull(attachmentsApi.deleteAttachments(ACCOUNT_UID, ATTACHMENT_TYPE, ENTITY_UID));
         getRequestWithValidation(HttpMethod.DELETE, ENTITY_LINKS, ACCOUNT_UID, ATTACHMENT_TYPE, ENTITY_UID);
-    }
-
-    @Test
-    public void testDeleteAttachment() throws Exception
-    {
-        assignResponse(HttpStatus.SC_OK, MediaType.APPLICATION_JSON, jsonResponse(null));
-
-        assertNotNull(attachmentsApi.deleteAttachment(ACCOUNT_UID, ATTACHMENT_TYPE, ENTITY_UID, ATTACHMENT_UID));
-        getRequestWithValidation(HttpMethod.DELETE, ENTITY_ATTACHMENT, ACCOUNT_UID, ATTACHMENT_TYPE, ENTITY_UID, ATTACHMENT_UID);
     }
 }
