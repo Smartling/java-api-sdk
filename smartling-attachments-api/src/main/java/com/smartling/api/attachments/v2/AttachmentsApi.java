61a948e1c381cb993b836c3643fe53bba70018c2
Alexander Novak <alexn@archer-soft.com>
2021-04-15 15:16:40 +0300

    Keep only read operations in SDK

--- smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
+++ smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
@@ -1,15 +1,10 @@
 package com.smartling.api.attachments.v2;
 
 import com.smartling.api.attachments.v2.pto.AttachmentPTO;
-import com.smartling.api.attachments.v2.pto.EntityAttachmentsPTO;
-import com.smartling.api.attachments.v2.pto.EntityLinkCommand;
 import com.smartling.api.v2.response.ListResponse;
-import com.smartling.api.v2.response.EmptyData;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,15 +15,7 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface AttachmentsApi
 {
-    @PUT
-    @Path("/accounts/{accountUid}/{type}")
-    ListResponse<EntityAttachmentsPTO> linkAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, EntityLinkCommand command);
-
     @GET
     @Path("/accounts/{accountUid}/{type}/{entityUid}")
     ListResponse<AttachmentPTO> entityAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid);
-
-    @DELETE
-    @Path("/accounts/{accountUid}/{type}/{entityUid}")
-    EmptyData deleteAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid);
 }
