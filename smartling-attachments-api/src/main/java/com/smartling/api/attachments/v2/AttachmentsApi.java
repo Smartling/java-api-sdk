bcec8318005152a4bdaa183b5727a5bb71ad4874
Oleksandr Pashynskyi <apashinskiy@smartling.com>
2023-03-10 15:56:40 +0200

    [TCM-3932] Add attachment upload api.

--- smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
+++ smartling-attachments-api/src/main/java/com/smartling/api/attachments/v2/AttachmentsApi.java
@@ -1,14 +1,14 @@
 package com.smartling.api.attachments.v2;
 
 import com.smartling.api.attachments.v2.pto.AttachmentPTO;
+import com.smartling.api.attachments.v2.pto.AttachmentUploadPTO;
 import com.smartling.api.v2.response.ListResponse;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
 
 @Path("/attachments-api/v2")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,4 +18,9 @@ public interface AttachmentsApi
     @GET
     @Path("/accounts/{accountUid}/{type}/{entityUid}")
     ListResponse<AttachmentPTO> entityAttachments(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @PathParam("entityUid") String entityUid);
+
+    @POST
+    @Path("/accounts/{accountUid}/{type}/attachments")
+    @Consumes(MULTIPART_FORM_DATA)
+    AttachmentPTO uploadAttachment(@PathParam("accountUid") String accountUid, @PathParam("type") String type, @MultipartForm AttachmentUploadPTO attachmentUploadPTO);
 }
