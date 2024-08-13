3700ad8a7feca1c49b064d426892ea11e54e4b6c
Dmitry Netrebenko <dnetrebenko@smartling.com>
2024-08-13 15:00:13 +0300

    minor fix

--- smartling-files-api/src/main/java/com/smartling/api/files/v2/resteasy/ext/TranslatedFileMultipart.java
+++ smartling-files-api/src/main/java/com/smartling/api/files/v2/resteasy/ext/TranslatedFileMultipart.java
@@ -1,6 +1,7 @@
 package com.smartling.api.files.v2.resteasy.ext;
 
 import lombok.Getter;
+import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartInput;
 
 import javax.ws.rs.core.MediaType;
@@ -19,8 +20,10 @@ public class TranslatedFileMultipart
     TranslatedFileMetadata translatedFileMetadata;
     MultipartInput multipartInput;
 
-    void close()
+    public void close()
     {
+        if (null != fileBody)
+            IOUtils.closeQuietly(fileBody);
         if (null != multipartInput)
             multipartInput.close();
     }
